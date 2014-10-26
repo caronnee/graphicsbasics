9b2d271466c89f085c14fee185f662d3d393b822
caronnee <caronnee@b80dbee9-20d2-825e-b3ef-16b6087b64a3>
2014-10-26 13:46:31 +0000

    Added random generator

---
+++ trunk/RaySamplesGui/TracingGui/Sources/RandomNumber.h
@@ -0,0 +1,22 @@
+#ifndef __RANDOM__
+#define __RANDOM__
+
+#include <random>
+#include "Vector.h"
+
+class Rng
+{
+public:
+	Rng(int aSeed = 1234);
+
+	  float GetFloat();
+
+	  Vector4d GetVec3f();
+
+private:
+
+	std::mt19937_64 mRng;
+	std::uniform_real_distribution<float> mDistFloat;
+};
+
+#endif
