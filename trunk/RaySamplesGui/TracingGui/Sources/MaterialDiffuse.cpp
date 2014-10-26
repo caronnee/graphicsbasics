e0d824a19c6b22193c6ec306c4b147749870a227
caronnee <caronnee@b80dbee9-20d2-825e-b3ef-16b6087b64a3>
2014-10-26 13:55:41 +0000

    Added scalar thing

--- trunk/RaySamplesGui/TracingGui/Sources/MaterialDiffuse.cpp
+++ trunk/RaySamplesGui/TracingGui/Sources/MaterialDiffuse.cpp
@@ -3,14 +3,14 @@
 
 Vector4d MaterialDiffuse::GetDiffuse(const Vector4d & input, const Vector4d & output) const
 {
-	return _diffuseReflectance;
+	return _diffuseReflectance / PI;
 }
 
 MaterialDiffuse::MaterialDiffuse(const Vector4d & diffuseReflectance, const Vector4d & emmisive) : base(emmisive), _diffuseReflectance (diffuseReflectance)
 {
 }
 
-Vector4d MaterialDiffuse::EvalBrdf(const Vector4d & input, const Vector4d & output) const
+Vector4d MaterialDiffuse::EvalBrdf(const Vector4d & input, Vector4d & output, float & pdf) const
 {
 	return GetDiffuse(input,output);
 }
