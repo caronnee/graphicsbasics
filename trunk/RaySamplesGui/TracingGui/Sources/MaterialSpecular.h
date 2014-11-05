933237790634c33d77d477d7c991e6916de9dc07
caronnee <caronnee@b80dbee9-20d2-825e-b3ef-16b6087b64a3>
2014-11-05 19:10:47 +0000

    Fixed scene1

--- trunk/RaySamplesGui/TracingGui/Sources/MaterialSpecular.h
+++ trunk/RaySamplesGui/TracingGui/Sources/MaterialSpecular.h
@@ -10,8 +10,7 @@ class MaterialSpecular : public MaterialDiffuse
 public:	
   MaterialSpecular(const Vector4d & diffuse, const Vector4d & specular, const float & phongCoef);
 
-	Vector4d GetSpecular( const Vector4d & incoming, const Vector4d & normal ) const;
-
+	Vector4d GetSpecular(const Vector4d & incoming, const Vector4d & normal, const Vector4d & output) const;
 	Vector4d EvalBrdf(const Vector4d & input,const Vector4d & normal, Vector4d & output) const;
 };
 #endif
