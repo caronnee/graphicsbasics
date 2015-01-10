2befcdf5f2af8dabd8cea4c66d17d30e5af53951
caronnee <caronnee@b80dbee9-20d2-825e-b3ef-16b6087b64a3>
2015-01-10 17:45:11 +0000

    removed bouncer

--- RaySamplesGui/TracingGui/Sources/renderer.h
+++ RaySamplesGui/TracingGui/Sources/renderer.h
@@ -53,19 +53,9 @@ public:
 	// resets 
 	virtual void Init() = 0;
 
-	// return new direction
-  virtual bool Bounce(Ray & ray, Intersection & section, Vector4d& throughput, float & pdf) = 0;
+	// return new direction and directional pdf that this was generated with
+  virtual bool Bounce(Ray & ray, Intersection & section, Vector4d& throughput) = 0;
 
-};
-
-class FiniteBouncer : public Bouncer
-{
-	int _maxBounces;
-	int _counter;
-public:
-	FiniteBouncer( int bounces );
-	void Init();
-  bool Bounce( Ray & ray, Intersection & section, Vector4d& throughput, float & pdf);
 };
 
 class Renderer
